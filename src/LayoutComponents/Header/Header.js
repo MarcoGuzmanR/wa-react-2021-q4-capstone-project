e25fa2b037fa7818a5816e1971b5a986190c836e
Marco Guzmán <marco.guzman2803@gmail.com>
2021-10-14 23:40:36 -0500

    First Header and Footer components with basic config for styling

---
+++ src/LayoutComponents/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header>
+      <h2 className={styles.color}>Logo</h2>
+      <nav>
+        <form>
+          <input type="search" placeholder="Search" />
+          <input type="submit" value="Search" />
+        </form>
+        <div>Shopping cart icon</div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
