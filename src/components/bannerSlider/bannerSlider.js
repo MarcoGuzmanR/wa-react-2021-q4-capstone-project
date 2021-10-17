6698219520be6a5995f667a7263fe62982300351
Marco Guzmán <marco.guzman2803@gmail.com>
2021-10-17 00:04:19 -0500

    Added banner slider component

--- src/components/bannerSlider/bannerSlider.js
+++ src/components/bannerSlider/bannerSlider.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import SimpleImageSlider from "react-simple-image-slider";
 
 function BannerSlider({ bannersList }) {
+  const images = bannersList.map((banner) => {
+    return {
+      url: banner.data.main_image.url
+    };
+  });
+
   return (
     <div>
-    Banner
+      <SimpleImageSlider
+        width={'100%'}
+        height={705}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+      />
     </div>
   );
 }
+
+BannerSlider.propTypes = {
+  bannersList: propTypes.array.isRequired
+};
 
 export default BannerSlider;
