414fac804c79932cfbeeb8b40388fa5505baf35a
Marco Guzmán <marco.guzman2803@gmail.com>
2021-10-16 21:48:13 -0500

    Added couple of styles to the category card component

--- src/components/categoryCard/categoryCard.js
+++ src/components/categoryCard/categoryCard.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import styles from './categoryCard.module.css'
 
 function CategoryCard({ category }) {
   const { data } = category;
   const { main_image } = data;
 
   return (
-    <div>
-      <img
-        src={main_image.url}
-        alt={main_image.alt}
-        height={main_image.dimensions.height / 3 }
-        width={main_image.dimensions.width / 3} />
-      <h3>{data.name}</h3>
+    <div className={styles.container}>
+      <a href={category.href}>
+        <img
+          src={main_image.url}
+          alt={main_image.alt}
+          height={main_image.dimensions.height / 3 }
+          width={main_image.dimensions.width / 3} />
+        <h3>{data.name}</h3>
+      </a>
     </div>
   );
 }
