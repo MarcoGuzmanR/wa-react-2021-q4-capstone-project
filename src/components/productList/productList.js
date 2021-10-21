8f77c5f9c01e6fee643dcbc779485144ec2be6be
Marco Guzmán <marco.guzman2803@gmail.com>
2021-10-20 23:02:35 -0500

    Added first approach for filtering the products

--- src/components/productList/productList.js
+++ src/components/productList/productList.js
@@ -11,21 +11,53 @@ function ProductList() {
       id: category.id,
       name: category.data.name
     }
- });
+  });
 
   const { results: mockedProducts } = productsRawData;
+
+  const [products, setProducts] = React.useState(mockedProducts);
+  const [filters, setFilters] = React.useState([]);
+
+  function handleFilterChange(filter) {
+    setFilters(prevFilters => {
+      const filterApplied = prevFilters.find(prevFilter => prevFilter.id === filter.id);
+
+      if (filterApplied) {
+        return prevFilters.filter(prevFilter => prevFilter.id !== filter.id);
+      }
+
+      return [...prevFilters, filter];
+    });
+  }
+
+  React.useEffect(() => {
+    const filterIds = filters.map(filter => filter.id);
+    const filteredProducts = mockedProducts.filter((product) => {
+      return filterIds.includes(product.data.category.id);
+    });
+
+    if (filterIds.length) {
+      return setProducts(filteredProducts);
+    }
+
+    setProducts(mockedProducts);
+  }, [filters, mockedProducts]);
 
   return (
     <div className={styles.container}>
       <aside>
-        {categoryFilters.map(({id, name}) => (
-          <div key={id} className={styles['filter-container']}>
-            <button className="btn-secondary">{name}</button>
+        {categoryFilters.map((filter) => (
+          <div key={filter.id} className={styles['filter-container']}>
+            <button
+              className="btn-secondary"
+              onClick={() => handleFilterChange(filter)}>
+              {filter.name}
+            </button>
           </div>
         ))}
       </aside>
       <section>
-        <ProductsGrid title="Products" productsList={mockedProducts} />
+        <ProductsGrid title="Products" productsList={products} />
       </section>
     </div>
   );
