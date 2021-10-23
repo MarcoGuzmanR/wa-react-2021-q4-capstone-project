0be502cfdf15e56e1992d6105bbfbe42d3e8edf5
Marco Guzmán <marco.guzman2803@gmail.com>
2021-10-23 13:09:40 -0500

    Added Map structure for the category data in the products

--- src/components/productCard/productCard.js
+++ src/components/productCard/productCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './productCard.module.css'
 import propTypes from 'prop-types';
 
-function ProductCard({ product }) {
+function ProductCard({ product, categoriesMap }) {
   const { data } = product;
   const { mainimage } = data;
 
@@ -18,7 +18,7 @@ function ProductCard({ product }) {
       </a>
       <div className={styles.details}>
         <p className={styles.category}>
-          <b>{data.category.slug}</b>
+          <b>{categoriesMap.get(data.category.id)}</b>
         </p>
         <p><b>${data.price.toFixed(2)}</b></p>
       </div>
@@ -40,11 +40,12 @@ ProductCard.propTypes = {
         url: propTypes.string.isRequired,
       }),
       category: propTypes.shape({
-        slug: propTypes.string,
+        id: propTypes.string,
       }),
       price: propTypes.number
     })
-  })
+  }),
+  categoriesMap: propTypes.object.isRequired
 };
 
 export default ProductCard;
