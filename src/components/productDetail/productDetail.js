1f5936fc57941751bdf7bd0c392d3862cd4df254
Marco Guzmán <marco.guzman2803@gmail.com>
2021-10-28 22:15:30 -0500

    Added first elements of productDetails component

--- src/components/productDetail/productDetail.js
+++ src/components/productDetail/productDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './productDetail.module.css'
+import LoaderSpinner from '../common/loaderSpinner';
 import { useParams } from 'react-router-dom';
 import propTypes from 'prop-types';
 
@@ -11,6 +12,7 @@ function ProductDetail() {
 
   const { data, isLoading } = useCustomResponseAPI(propsCall);
   const [product, setProduct] = React.useState();
+  console.log(product);
 
   React.useEffect(() => {
     if (!isLoading) {
@@ -20,7 +22,38 @@ function ProductDetail() {
 
   return (
     <div className={styles.container}>
-      <pre>{JSON.stringify(product)}</pre>
+      {!isLoading && product ?
+        <div className={styles['main-container']}>
+          <div className={styles['top-container']}>
+            <div className={styles['gallery-container']}>
+              <h2>{product.data.name}</h2>
+            </div>
+            <div className={styles['details-container']}>
+              <label htmlFor="price">Price: ${product.data.price.toFixed(2)}</label>
+              <label htmlFor="sku">SKU: {product.data.sku}</label>
+              <label htmlFor="category">Category: {product.data.category.id}</label>
+              {product.tags.map((tag, index) => (
+                <label key={index}>{tag}</label>
+              ))}
+              <p>{product.data.short_description}</p>
+              <label htmlFor="numberItems">Quantity:</label>
+              <input name="numberItems" type="number" />
+              <input className="btn-secondary" type="button" value="Add to Cart" />
+            </div>
+          </div>
+          <div className={styles['specs-container']}>
+            <h3>Specs:</h3>
+            <ul>
+            {product.data.specs.map((spec, index) => (
+              <li key={index}>
+                <label><b>{spec.spec_name}:</b> {spec.spec_value}</label>
+              </li>
+            ))}
+            </ul>
+          </div>
+        </div> :
+        <LoaderSpinner />
+      }
     </div>
   );
 }
